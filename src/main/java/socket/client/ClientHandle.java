c8eccb0591d923453192301682def26c75650d67
Sagar <patelsagar913@gmail.com>
2025-07-03 18:42:19 +0530

    yz1

--- src/main/java/socket/client/ClientHandle.java
+++ src/main/java/socket/client/ClientHandle.java
@@ -29,47 +29,48 @@ public class ClientHandle implements Runnable {
 
         try(OutputStream writer = clientSocket.getOutputStream()) {
             while(true) {
-                RESPArrayParser parser = RESPArrayParser.getBuilder().setInputStream(clientSocket.getInputStream()).build();
-                Object commands = parser.parse();
-                if (commands instanceof List) {
-                    String written = "";
-                    List<Object> cmdParts = (List<Object>) commands;
-                    String commandName = cmdParts.get(currentCommandIndex.get()).toString();
-                    if(commandName.equalsIgnoreCase("ping")) {
-                        written="+PONG\r\n";
-                    }else if(commandName.equalsIgnoreCase("echo")){
-                        String value=cmdParts.get(currentCommandIndex.addAndGet(1)).toString();
-                        written="$"+value.length()+"\r\n"+value+"\r\n";
-                    }else if (commandName.equalsIgnoreCase("set")) {
-                        String key = cmdParts.get(currentCommandIndex.addAndGet(1)).toString();
-                        String value = cmdParts.get(currentCommandIndex.addAndGet(1)).toString();
-                        if(currentCommandIndex.addAndGet(1)<cmdParts.size()){
-                            TimeUnit timeUnit = TimeUnit.MILLISECONDS;
-                            if(cmdParts.get(currentCommandIndex.get()).equals("px")){
-                                timeUnit=TimeUnit.MILLISECONDS;
-                            }else if(cmdParts.get(currentCommandIndex.get()).equals("ex")){
-                                timeUnit=TimeUnit.SECONDS;
-                            }
-
-                            long delay=Long.parseLong(cmdParts.get(currentCommandIndex.addAndGet(1)).toString());
-                            expiringMap.put(key,value,delay,timeUnit);
-                        }else{
-                            expiringMap.put(key, value);
-                        }
-                        written += "+OK\r\n";
-                    }else if (commandName.equalsIgnoreCase("get")) {
-                        written = "$";
-                        String key = cmdParts.get(currentCommandIndex.addAndGet(1)).toString();
-                        if (expiringMap.containsKey(key)) {
-                            String value = expiringMap.get(key);
-                            written += value.length() + "\r\n" + value + "\r\n";
-                        } else {
-                            written += "-1\r\n";
-                        }
-                    }
-                    writer.write(written.getBytes());
-                    writer.flush();
+                Object commands=null;
+                synchronized (this) {
+                    RESPArrayParser parser = RESPArrayParser.getBuilder().setInputStream(clientSocket.getInputStream()).build();
+                    commands = parser.parse();
                 }
+                String written = "";
+                List<Object> cmdParts = (List<Object>) commands;
+                String commandName = cmdParts.get(currentCommandIndex.get()).toString();
+                if(commandName.equalsIgnoreCase("ping")) {
+                    written="+PONG\r\n";
+                }else if(commandName.equalsIgnoreCase("echo")){
+                    String value=cmdParts.get(currentCommandIndex.addAndGet(1)).toString();
+                    written="$"+value.length()+"\r\n"+value+"\r\n";
+                }else if (commandName.equalsIgnoreCase("set")) {
+                    String key = cmdParts.get(currentCommandIndex.addAndGet(1)).toString();
+                    String value = cmdParts.get(currentCommandIndex.addAndGet(1)).toString();
+                    if(currentCommandIndex.addAndGet(1)<cmdParts.size()){
+                        TimeUnit timeUnit = TimeUnit.MILLISECONDS;
+                        if(cmdParts.get(currentCommandIndex.get()).equals("px")){
+                            timeUnit=TimeUnit.MILLISECONDS;
+                        }else if(cmdParts.get(currentCommandIndex.get()).equals("ex")){
+                            timeUnit=TimeUnit.SECONDS;
+                        }
+
+                        long delay=Long.parseLong(cmdParts.get(currentCommandIndex.addAndGet(1)).toString());
+                        expiringMap.put(key,value,delay,timeUnit);
+                    }else{
+                        expiringMap.put(key, value);
+                    }
+                    written += "+OK\r\n";
+                }else if (commandName.equalsIgnoreCase("get")) {
+                    String key = cmdParts.get(currentCommandIndex.addAndGet(1)).toString();
+                    written = "$";
+                    if (expiringMap.containsKey(key)) {
+                        String value = expiringMap.get(key);
+                        written += value.length() + "\r\n" + value + "\r\n";
+                    } else {
+                        written += "-1\r\n";
+                    }
+                }
+                writer.write(written.getBytes());
+                writer.flush();
             }
 
         }catch (Exception e) {
