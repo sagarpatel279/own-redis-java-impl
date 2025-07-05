de3764e5f4ede10579aa4e4d24905f74a443eaac
sagarpatel279 <patelsagar913@gmail.com>
2025-07-05 15:40:13 +0530

    handling command design changed

--- src/main/java/components/handlers/ClientHandler.java
+++ src/main/java/components/handlers/ClientHandler.java
@@ -4,6 +4,7 @@ import static resp.constants.RESPParserConstants.*;
 import static resp.constants.RESPCommandsConstants.*;
 import static resp.constants.RESPEncodingConstants.*;
 
+import lombok.RequiredArgsConstructor;
 import resp.parser.RESPArrayParser;
 import sockets.client.Client;
 
@@ -11,61 +12,79 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.SocketTimeoutException;
-
+@RequiredArgsConstructor
 public class ClientHandler implements Runnable {
     private final Client client;
-    public ClientHandler(Client client) {
-        this.client = client;
-    }
+    private final CommandHandler commandHandler;
+
     @Override
     public void run() {
-        handleClient();
-    }
-    private void handleClient() {
-        System.out.println("Client connected: " + client.getSocket().getRemoteSocketAddress());
-
-        try(OutputStream writer = client.getOutputStream();
-                        InputStream stream=client.getInputStream()) {
-            while(client.getSocket().isConnected()) {
-                if(stream.available()<=0)continue;
-                System.out.println("Handled By Thread: "+Thread.currentThread().getName());
+        try (InputStream stream = client.getInputStream(); OutputStream writer = client.getOutputStream()) {
+            client.getSocket().setSoTimeout(20000);
+            while (client.getSocket().isConnected()) {
+                if (stream.available() <= 0) continue;
                 RESPArrayParser parser = RESPArrayParser.getBuilder().setInputStream(stream).build();
-                client.getCommandHandler().setCommandQueue(parser.getCommandList());
-                String commandName = client.getCommandHandler().pullCommand().toString();
-                String response;
-                if(commandName.equalsIgnoreCase(C_PING)) {
-                    response =  client.getCommandHandler().handlePingCommand();
-                }else if(commandName.equalsIgnoreCase(C_ECHO)){
-                    response = client.getCommandHandler().handleEchoCommand();
-                }else if (commandName.equalsIgnoreCase(C_SET)) {
-                    response = client.getCommandHandler().handleSetCommand();
-                }else if (commandName.equalsIgnoreCase(C_GET)) {
-                    response = client.getCommandHandler().handleGetCommand();
-                }else{
-                    response= BULK_STRING+NULL_VALUE;
-                }
+                commandHandler.setCommandQueue(parser.getCommandList());
+                String commandName = commandHandler.pullCommand().toString();
+                String response = commandHandler.handleCommand(commandName);
                 writer.write(response.getBytes());
                 writer.flush();
             }
-
-        }catch (SocketTimeoutException ste){
-            System.out.println("Time out for Client: "+client.getSocket().getRemoteSocketAddress());
-            try {
-                client.getSocket().getOutputStream().write("Timeout..please reconnect".getBytes());
-            } catch (IOException e) {
-                throw new RuntimeException(e.getMessage());
-            }
-        }catch (Exception e) {
-            System.out.println("Client handler error: " + e.getMessage());
-        }finally {
-            try {
-                if(!client.getSocket().isClosed()) {
-                    System.out.println("Client: "+client.getSocket().getRemoteSocketAddress()+" has been closed...");
-                    client.getSocket().close();
-                }
-            } catch (IOException e) {
-                System.out.println("Close error: " + e.getMessage());
-            }
+        } catch (SocketTimeoutException ste) {
+            System.out.println("Client Timeout: " + client.getClientId());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+//    @Override
+//    public void run() {
+//        handleClient();
+//    }
+//    private void handleClient() {
+//        System.out.println("Client connected: " + client.getSocket().getRemoteSocketAddress());
+//
+//        try(OutputStream writer = client.getOutputStream();
+//                        InputStream stream=client.getInputStream()) {
+//            while(client.getSocket().isConnected()) {
+//                if(stream.available()<=0)continue;
+//                System.out.println("Handled By Thread: "+Thread.currentThread().getName());
+//                RESPArrayParser parser = RESPArrayParser.getBuilder().setInputStream(stream).build();
+//                client.getCommandHandler().setCommandQueue(parser.getCommandList());
+//                String commandName = client.getCommandHandler().pullCommand().toString();
+//                String response;
+//                if(commandName.equalsIgnoreCase(C_PING)) {
+//                    response =  client.getCommandHandler().handlePingCommand();
+//                }else if(commandName.equalsIgnoreCase(C_ECHO)){
+//                    response = client.getCommandHandler().handleEchoCommand();
+//                }else if (commandName.equalsIgnoreCase(C_SET)) {
+//                    response = client.getCommandHandler().handleSetCommand();
+//                }else if (commandName.equalsIgnoreCase(C_GET)) {
+//                    response = client.getCommandHandler().handleGetCommand();
+//                }else{
+//                    response= BULK_STRING+NULL_VALUE;
+//                }
+//                writer.write(response.getBytes());
+//                writer.flush();
+//            }
+//
+//        }catch (SocketTimeoutException ste){
+//            System.out.println("Time out for Client: "+client.getSocket().getRemoteSocketAddress());
+//            try {
+//                client.getSocket().getOutputStream().write("Timeout..please reconnect".getBytes());
+//            } catch (IOException e) {
+//                throw new RuntimeException(e.getMessage());
+//            }
+//        }catch (Exception e) {
+//            System.out.println("Client handler error: " + e.getMessage());
+//        }finally {
+//            try {
+//                if(!client.getSocket().isClosed()) {
+//                    System.out.println("Client: "+client.getSocket().getRemoteSocketAddress()+" has been closed...");
+//                    client.getSocket().close();
+//                }
+//            } catch (IOException e) {
+//                System.out.println("Close error: " + e.getMessage());
+//            }
+//        }
+//    }
 }
--- src/main/java/components/handlers/CommandHandler.java
+++ src/main/java/components/handlers/CommandHandler.java
@@ -1,6 +1,9 @@
 package components.handlers;
 
 import components.repos.ExpiringMap;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -11,76 +14,121 @@ import static resp.constants.RESPCommandsConstants.*;
 import static resp.constants.RESPEncodingConstants.*;
 import static resp.constants.RESPParserConstants.*;
 
+@Component
+@Scope("prototype")
+@RequiredArgsConstructor
 public class CommandHandler {
-    private final ExpiringMap<Object,Object> expiringMap;
-    private final Queue<Object> commandQueue=new LinkedList<>();
-    public CommandHandler(ExpiringMap<Object,Object> expiringMap){
-        this.expiringMap=expiringMap;
-    }
-    public void setCommandQueue(List<String> commandList) {
-        System.out.println("Command List: "+commandList);
-        this.commandQueue.addAll(commandList);
-    }
+    private final ExpiringMap<Object, Object> expiringMap;
+    private final Queue<Object> commandQueue = new LinkedList<>();
+//
+//    public void setCommandQueue(List<String> commandList) {
+//        System.out.println("Command List: "+commandList);
+//        this.commandQueue.addAll(commandList);
+//    }
+//
+//    public String handleEchoCommand(){
+//        String returnValue= pullCommand().toString();
+//        return BULK_STRING+returnValue.length()+C_CRLF+returnValue+C_CRLF;
+//    }
+//
+//    public String handlePingCommand(){
+//        return SIMPLE_STRING+C_PONG+C_CRLF;
+//    }
+//
+//    public Object pullCommand(){
+//       if(!isCommandExist())
+//            throw new NoSuchElementException("Queue is Empty");
+//        return commandQueue.poll();
+//    }
+//    public Object fetchCommand(){
+//        if(!isCommandExist())
+//            throw new NoSuchElementException("Queue is Empty");
+//        return commandQueue.peek();
+//    }
+//    public boolean isCommandExist(){
+//        return !commandQueue.isEmpty();
+//    }
+//    public int countCommands(){
+//        return commandQueue.size();
+//    }
+//    public String handleSetCommand(){
+//        Object key = pullCommand();
+//        Object value = pullCommand();
+//        if(isCommandExist()) {
+//            String expiryType= pullCommand().toString();
+//            TimeUnit timeUnit;
+//            if (expiryType.equalsIgnoreCase(C_PX)) {
+//                timeUnit = TimeUnit.MILLISECONDS;
+//            } else if (expiryType.equalsIgnoreCase(C_EX)) {
+//                timeUnit = TimeUnit.SECONDS;
+//            } else {
+//                throw new IllegalArgumentException("Expiry type not supported: " + expiryType);
+//            }
+//            long delay;
+//            try {
+//                delay = Long.parseLong(pullCommand().toString());
+//            } catch (NumberFormatException e) {
+//                throw new IllegalArgumentException("Invalid expiry time" , e);
+//            }
+//            expiringMap.put(key, value, delay, timeUnit);
+//        } else {
+//            expiringMap.put(key, value);
+//        }
+//        return SIMPLE_STRING + C_OK + C_CRLF;
+//    }
+//    public String handleGetCommand(){
+//        String response = BULK_STRING;
+//        Object key = pullCommand();
+//        if (expiringMap.containsKey(key)) {
+//            String value = expiringMap.get(key).toString();
+//            response += value.length() + C_CRLF + value + C_CRLF;
+//        } else {
+//            response += NULL_VALUE;
+//        }
+//        return response;
+//    }
+public void setCommandQueue(List<String> commandList) {
+    this.commandQueue.clear();
+    this.commandQueue.addAll(commandList);
+}
 
-    public String handleEchoCommand(){
-        String returnValue= pullCommand().toString();
-        return BULK_STRING+returnValue.length()+C_CRLF+returnValue+C_CRLF;
-    }
-
-    public String handlePingCommand(){
-        return SIMPLE_STRING+C_PONG+C_CRLF;
-    }
-
-    public Object pullCommand(){
-       if(!isCommandExist())
-            throw new NoSuchElementException("Queue is Empty");
+    public Object pullCommand() {
         return commandQueue.poll();
     }
-    public Object fetchCommand(){
-        if(!isCommandExist())
-            throw new NoSuchElementException("Queue is Empty");
-        return commandQueue.peek();
+
+    public String handleCommand(String commandName) {
+        return switch (commandName.toUpperCase()) {
+            case C_PING -> SIMPLE_STRING + C_PONG + C_CRLF;
+            case C_ECHO -> {
+                String echoVal = pullCommand().toString();
+                yield BULK_STRING + echoVal.length() + C_CRLF + echoVal + C_CRLF;
+            }
+            case C_SET -> handleSetCommand();
+            case C_GET -> handleGetCommand();
+            default -> BULK_STRING + NULL_VALUE;
+        };
     }
-    public boolean isCommandExist(){
-        return !commandQueue.isEmpty();
-    }
-    public int countCommands(){
-        return commandQueue.size();
-    }
-    public String handleSetCommand(){
+
+    private String handleSetCommand() {
         Object key = pullCommand();
         Object value = pullCommand();
-        if(isCommandExist()) {
-            String expiryType= pullCommand().toString();
-            TimeUnit timeUnit;
-            if (expiryType.equalsIgnoreCase(C_PX)) {
-                timeUnit = TimeUnit.MILLISECONDS;
-            } else if (expiryType.equalsIgnoreCase(C_EX)) {
-                timeUnit = TimeUnit.SECONDS;
-            } else {
-                throw new IllegalArgumentException("Expiry type not supported: " + expiryType);
-            }
-            long delay;
-            try {
-                delay = Long.parseLong(pullCommand().toString());
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("Invalid expiry time" , e);
-            }
+        if (!commandQueue.isEmpty()) {
+            String expiryType = pullCommand().toString();
+            TimeUnit timeUnit = expiryType.equalsIgnoreCase(C_PX) ? TimeUnit.MILLISECONDS : TimeUnit.SECONDS;
+            long delay = Long.parseLong(pullCommand().toString());
             expiringMap.put(key, value, delay, timeUnit);
         } else {
             expiringMap.put(key, value);
         }
         return SIMPLE_STRING + C_OK + C_CRLF;
     }
-    public String handleGetCommand(){
-        String response = BULK_STRING;
+
+    private String handleGetCommand() {
         Object key = pullCommand();
         if (expiringMap.containsKey(key)) {
             String value = expiringMap.get(key).toString();
-            response += value.length() + C_CRLF + value + C_CRLF;
-        } else {
-            response += NULL_VALUE;
+            return BULK_STRING + value.length() + C_CRLF + value + C_CRLF;
         }
-        return response;
+        return BULK_STRING + NULL_VALUE;
     }
 }
