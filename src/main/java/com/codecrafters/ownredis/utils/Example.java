6c5308e8fba706e9a930a3d8c5ebf5ed4f685338
sagarpatel279 <patelsagar913@gmail.com>
2025-07-22 10:06:51 +0530

    practice on example

--- src/main/java/com/codecrafters/ownredis/utils/Example.java
+++ src/main/java/com/codecrafters/ownredis/utils/Example.java
@@ -1,17 +1,41 @@
 package com.codecrafters.ownredis.utils;
 
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ForkJoinPool;
+import lombok.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 public class Example {
-    public static void main(String[] args) throws InterruptedException {
-        
-//        CompletableFuture
-    }
 
+    public static void main(String[] args) {
+        User user = new User(1L, "xyz123", "xyz", "xyz@gmail.com");
+        System.out.println("User: " + user);
+
+        System.out.println("Class Name: "+UserMapper.userMapperInstance.getClass().getName());
+        UserDTO userDTO = UserMapper.userMapperInstance.userToUserDTO(user);
+        System.out.println("UserDTO: " + userDTO);
+    }
 }
+
+
+record UserDTO(Long id, String username, String email) {
+}
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+class User {
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+}
+
+@Mapper(componentModel = "spring")
+interface UserMapper {
+    UserMapper userMapperInstance= Mappers.getMapper(UserMapper.class);
+    UserDTO userToUserDTO(User user);
+    User userDtoToUser(UserDTO userDTO);
+}
+
